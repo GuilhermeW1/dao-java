bf713b0b86bd294c3ba48d4b149c5cba148cbab9
Guilherme Weissheimer <guilhermeweissheimer.w1@gmail.com>
2025-01-04 16:50:52 -0300

    seller entity

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -2,10 +2,14 @@ package application;
 
 import db.DB;
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
         Department obj = new Department(1, "Books");
-        System.out.println(obj);
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", LocalDate.of(2000, 07, 24), 3000.0, obj);
+        System.out.println(seller);
     }
 }
